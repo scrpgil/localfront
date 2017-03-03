c9f1d89282a4a817c5360369605816191994be87
eskey <esplants@outlook.com>
2017-03-03 23:47:09 +0900

    [add]ルーティング設定見直し＆コンテントタイプ自動設定
    ルーティング設定を見直しました

--- controllers/default.go
+++ controllers/default.go
@@ -9,30 +9,15 @@ type MainController struct {
 	beego.Controller
 }
 
-func (c *MainController) GetVideoFile() {
-	beego.Info("GetVideoFile")
-	directory := string([]byte(c.Ctx.Input.Param(":directory")))
-	key := string([]byte(c.Ctx.Input.Param(":key")))
-	file := string([]byte(c.Ctx.Input.Param(":file")))
-	data, err := ioutil.ReadFile("files/" + directory + "/" + key + "/" + file)
+func (c *MainController) GetFile() {
+	path := c.Ctx.Input.Param(":path")
+	ext := c.Ctx.Input.Param(":ext")
+	data, err := ioutil.ReadFile("files/" + path + "." + ext)
 	if err != nil {
+		c.Ctx.ResponseWriter.WriteHeader(404)
 		return
 	}
-	c.Ctx.Output.Header("Content-Type", "application/mp4")
-	c.Ctx.Output.Body(data)
-	return
-}
-
-func (c *MainController) GetVideoThumbnail() {
-	beego.Info("GetVideoThumbnail")
-	directory := string([]byte(c.Ctx.Input.Param(":directory")))
-	key := string([]byte(c.Ctx.Input.Param(":key")))
-	file := string([]byte(c.Ctx.Input.Param(":file")))
-	data, err := ioutil.ReadFile("files/" + directory + "/" + key + "/img/" + file)
-	if err != nil {
-		return
-	}
-	c.Ctx.Output.Header("Content-Type", "application/img")
+	c.Ctx.Output.ContentType(ext)
 	c.Ctx.Output.Body(data)
 	return
 }
